7377f14412d559b1bc23b491155d9ca8d8847228
yogenders <yogender.singh.wd@gmail.com>
2016-10-13 22:08:46 +0530

    Accessing movie data using Intent Service: Part 1

---
+++ app/src/main/java/com/example/yogenders/movinationfinal/Services/WebDataExtractService.java
@@ -0,0 +1,21 @@
+package com.example.yogenders.movinationfinal.Services;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+public class WebDataExtractService extends IntentService {
+
+    public WebDataExtractService() {
+        super("WebDataExtractService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+
+
+
+
+}
